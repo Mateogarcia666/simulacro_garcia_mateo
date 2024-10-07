9065c69defb7804a87810f52f1058644c504929f
Mateo Garcia <mateoriver1@gmail.com>
2024-10-07 12:02:54 -0300

    simulacro con funciones importadas

---
+++ simulacro.py
@@ -0,0 +1,19 @@
+from simulacrofunc import *
+
+inventario = []
+while True:
+    menu()
+    opcion = input("Ingrese una opcion: (1,2,3,4,5,6):")
+
+    if opcion == '1':
+        cargar_inventario()
+    elif opcion == '2':
+        buscar_producto()
+    elif opcion == '3':
+        ordenar_inventario(inventario)
+    elif opcion == '4':
+        producto_mas_caro()
+        producto_mas_barato()
+    elif opcion == '5':
+        producto_superior_15000()
+    
